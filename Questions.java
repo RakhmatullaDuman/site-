c6601c275e73bc92508fc542aba8b421c4125877
RakhmatullaDuman <120190417+RakhmatullaDuman@users.noreply.github.com>
2022-12-10 10:05:00 +0600

    Add files via upload

---
+++ Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: Duman");
+        System.out.println("Surname: Rahmatulla");
+        System.out.println("Id: 220103077");
+        System.out.println("Hobby: Sleeping");
+        System.out.println("How much do you like ICT(from 1-10)): 10000");
+        System.out.println("Rate this task from 1 to 10: 1000");
+        System.out.println("My favorite color?: black");
+    }
+}
